87345117366417b3059b86e99cc8c780fd442ffb
Helge Stegemoen <hstegemo@hotmail.com>
2023-07-23 20:19:02 +0200

    Improving unit tests

--- ECommerce.Api.Products.Tests/ProductsServiceTest.cs
+++ ECommerce.Api.Products.Tests/ProductsServiceTest.cs
@@ -33,6 +33,45 @@ namespace ECommerce.Api.Products.Tests
             Assert.Null(product.ErrorMessage);
         }
 
+        [Fact]
+        public async Task GetProductsReturnsProductUsingValidId()
+        {
+            var options = new DbContextOptionsBuilder<ProductsDbContext>()
+                .UseInMemoryDatabase(nameof(GetProductsReturnsProductUsingValidId))
+                .Options;
+            var dbContext = new ProductsDbContext(options);
+            CreateProducts(dbContext);
+            var productProfile = new ProductProfile(); //class . {CreateMap<Db.Product,Models.Product>();}
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(productProfile));
+            var mapper = new Mapper(configuration); // Create mapper using the configuration
+            var productsProvider = new ProductsProvider(dbContext, null, mapper);
+
+            var product = await productsProvider.GetProductAsync(1);
+            Assert.True(product.IsSuccess);
+            Assert.NotNull(product.Product);
+            Assert.True(product.Product.Id == 1);
+            Assert.Null(product.ErrorMessage);
+        }
+
+        [Fact]
+        public async Task GetProductsReturnsProductUsingInValidId()
+        {
+            var options = new DbContextOptionsBuilder<ProductsDbContext>()
+                .UseInMemoryDatabase(nameof(GetProductsReturnsProductUsingValidId))
+                .Options;
+            var dbContext = new ProductsDbContext(options);
+            CreateProducts(dbContext);
+            var productProfile = new ProductProfile(); //class . {CreateMap<Db.Product,Models.Product>();}
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(productProfile));
+            var mapper = new Mapper(configuration); // Create mapper using the configuration
+            var productsProvider = new ProductsProvider(dbContext, null, mapper);
+
+            var product = await productsProvider.GetProductAsync(-1);
+            Assert.False(product.IsSuccess);
+            Assert.Null(product.Product);
+            Assert.NotNull(product.ErrorMessage); // need to know if the provider object is behaving the correct way
+        }
+
         private void CreateProducts(ProductsDbContext dbContext)
         {
             for (int i=1; i<=10; i++)
